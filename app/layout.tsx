a777821bee57289928beb23a089dde174a2419fa
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2023-11-21 14:45:10 +0100

    💅 increases min width for website

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} max-w-5xl mx-auto md:p-9 p-4`}>
+      <body className={`${inter.className} max-w-7xl mx-auto md:p-9 p-4`}>
         {children}
       </body>
     </html>
