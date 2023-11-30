e3699fb52b0d83ff6307c953928cbb57cd3950a7
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2023-11-30 12:25:04 +0100

    ✨ scrapes ep data using puppeteer

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,68 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+const puppeteer = require("puppeteer");
 
 let start = Date.now();
 
-async function getHtml(url) {
+const url = "https://vamosfalarsobremusica.com.br/eps-4-0/";
+
+async function openUrl(url) {
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: "1.png" });
+
+    const urls = await page.evaluate(() => {
+      const anchorEls = document.querySelectorAll("a");
+      const epUrls = Array.from(anchorEls)
+        .map((ep) => ep.href)
+        .filter((href) => href.includes("/lado-b"));
+
+      // removes duplicated urls
+      return Array.from(new Set(epUrls));
+    });
+
+    const epsData = await Promise.all(
+      urls.map(async (url) => {
+        const page = await browser.newPage();
+        await page.goto(url);
+        await page.waitForSelector(
+          ".elementor-widget-theme-post-featured-image"
+        );
+
+        const epData = await page.evaluate(() => {
+          const titleEl = document.querySelector(
+            ".elementor-heading-title.elementor-size-medium"
+          );
+          const title = titleEl.innerText;
+
+          const releaseDateEl = document.querySelector(
+            ".elementor-post-info__item--type-date"
+          );
+          const release_date = releaseDateEl.innerText;
+
+          const imgEl = document.querySelector(
+            ".elementor-widget-theme-post-featured-image img"
+          );
+          const img = imgEl.src;
+
+          const spotifyEl = document.querySelector("iframe");
+          const spotify_url = spotifyEl.src;
+
+          console.log({ title, releaseDate, img, spotifyUrl });
+          return { title, release_date, img, spotify_url };
+        });
+
+        return epData;
+      })
+    );
+
+    await browser.close();
+    console.log(epsData);
+    console.log(`Finished in: `, Date.now() - start);
   } catch (error) {
-    console.error("Error fetching HTML:", error);
-    return null;
+    console.error("Error opening URL:", error);
   }
 }
 
-function getEpUrls(html) {
-  const $ = cheerio.load(html);
-  const epUrls = $("a")
-    .map((i, el) => $(el).attr("href"))
-    .get()
-    .filter((href) => href && href.includes("/lado-b"));
-
-  console.log(`Finished in: `, Date.now() - start);
-  return Array.from(new Set(epUrls));
-}
-
-const url = "https://vamosfalarsobremusica.com.br/eps-4-0/";
-
-getHtml(url)
-  .then((html) => {
-    const epUrls = getEpUrls(html);
-    const requests = epUrls.map(async (url) => {
-      const res = await getHtml(url);
-      const $ = cheerio.load(res);
-      return {
-        url,
-        title: $(".elementor-heading-title.elementor-size-medium")
-          .first()
-          .text(),
-        date: $(".elementor-post-info__item--type-date").prop("innerText"),
-        cover_img_url: $(
-          ".elementor-widget-theme-post-featured-image img"
-        ).get()[0],
-      };
-    });
-    Promise.all(requests).then((data) => console.log(data));
-  })
-  .catch((error) => console.error(error));
-
-console.log("hi");
+openUrl(url);
+console.log("Fetching data...");
