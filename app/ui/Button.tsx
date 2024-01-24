72a0c2168c8f6223f6663997ee439b78acc1b7f1
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2024-01-24 13:46:44 -0300

    💅 decrease button text size

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 export function Button({ text, secondary }: ButtonProps) {
   const classes = clsx(
-    `text-sm h-8 px-6 font-light text-indigo-100 transition-colors duration-150 bg-slate-950 rounded-full focus:shadow-outline hover:opacity-75 `,
+    `text-xs h-8 px-7 font-light text-indigo-100 transition-colors duration-150 bg-slate-950 rounded-full focus:shadow-outline hover:opacity-75 `,
     { "border border-slate-950 bg-transparent text-slate-950": secondary }
   );
   return <button className={classes}>{text}</button>;
